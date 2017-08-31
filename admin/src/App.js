d73d98ea2d42e9e1929ef5a3da6fc09fe32acd09
dtonys@gmail.com <dtonys@gmail.com>
2017-08-31 11:25:03 -0700

    Figured out build problems by ejecting create-react-app and installing admin on rest via nom

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  simpleRestClient,
+  Admin,
+  Resource,
+  Delete,
+  fetchUtils,
+} from 'admin-on-rest';
+
+import {
+  UserList,
+  UserEdit,
+  UserCreate,
+} from './users';
+
+// import PlaceIcon from 'material-ui/svg-icons/maps/place';
+import PersonIcon from 'material-ui/svg-icons/social/person';
+
+const httpClient = (url, options = {}) => {
+  if (!options.headers) {
+    options.headers = new Headers({ Accept: 'application/json' });
+  }
+  // Pass this optoin into the `fetch` API to allow cookies to be sent to the server
+  options.credentials = 'include';
+  return fetchUtils.fetchJson(url, options);
+};
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Admin
+        restClient={simpleRestClient('http://localhost:8050/aor-api', httpClient )}
+        title="MapPlaces Admin"
+      >
+        <Resource
+          name="users"
+          icon={PersonIcon}
+          edit={UserEdit}
+          create={UserCreate}
+          remove={Delete}
+          list={UserList}
+        />
+      </Admin>
     );
   }
 }
